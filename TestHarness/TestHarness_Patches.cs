542949230b36bd68c1635711bc56f68c54a46f6c
maarxx <chris.lewark@gmail.com>
2023-04-22 00:03:24 -0400

    GetGoodwillGainForPrisonerRelease

---
+++ TestHarness/TestHarness_Patches.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using ModButtons;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace TestHarness
+{
+    [HarmonyPatch(typeof(Faction))]
+    [HarmonyPatch("GetGoodwillGainForPrisonerRelease")]
+    class Patch_Faction_GetGoodwillGainForPrisonerRelease
+    {
+        static void Postfix(Faction __instance, Pawn member, bool isHealthy, bool isInMentalState, int __result)
+        {
+            string noAidRelationsGainUntilTime = "";
+            int noAidRelationsGainUntilTick = member.mindState.noAidRelationsGainUntilTick;
+            if (noAidRelationsGainUntilTick > Find.TickManager.TicksGame)
+            {
+                int tickDiff = noAidRelationsGainUntilTick - Find.TickManager.TicksGame;
+                noAidRelationsGainUntilTime = " (" + tickDiff + " [" + tickDiff.ToStringTicksToPeriod(shortForm: true) + "])";
+            }
+            Log.Message("GGwGFPR: " + __instance.Name + ", " + member.LabelShort + ", " + isHealthy + ", " + isInMentalState + ", " + noAidRelationsGainUntilTick + noAidRelationsGainUntilTime + ", " + __result);
+        }
+    }
+}
